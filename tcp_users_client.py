f9a1b10bcf9b982dddf1718807e4165dce1c1c11
e-kovaleva <fullmoon010191@gmail.com>
2025-09-09 19:16:53 +0300

    5.7 Практика работы с TCP/IP протоколом

--- tcp_users_client.py
+++ tcp_users_client.py
@@ -6,7 +6,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
-message = 'Привет, сервер!'
+message = 'Как дела?'
 client_socket.send(message.encode())
 
 response = client_socket.recv(1024).decode()
--- tcp_users_server.py
+++ tcp_users_server.py
@@ -1,24 +1,28 @@
 import socket
 
 
+messages = []
+
+
 def server():
+    messages = []
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_address = ('localhost', 12345)
     server_socket.bind(server_address)
 
-    server_socket.listen(5)
+    server_socket.listen(10)
     print('Сервер запущен и ждет подключения...')
 
     while True: 
         client_socket, client_address = server_socket.accept()
-        print(f'Подключение от {client_address}')
+        print(f'Пользователь с адресом: {client_address} подключился к серверу')
 
         data = client_socket.recv(1024).decode()
-        print(f'Получено сообщение: {data}')
+        print(f'Пользователь с адресом: {client_address} отправил сообщение: {data}')
+        messages.append(data)
 
-        response = f'Сервер получил: {data}'
-        client_socket.send(response.encode())
+        client_socket.send('\n'.join(messages).encode())
 
         client_socket.close()
 
