fbe77bc38bb7e2f34da497259acb250558eac85c
e-kovaleva <fullmoon010191@gmail.com>
2025-09-21 14:57:14 +0300

    7.2 Практикуемся в применении Pydantic в API клиентах

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -2,9 +2,10 @@ from clients.users.public_users_client import get_public_users_client
 from clients.users.private_users_client import AuthenticationUserSchema
 from tools.fakers import get_random_email
 from clients.files.files_client import get_files_client
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
+from clients.courses.courses_client import get_courses_client
 from clients.users.users_schema import CreateUsersRequestSchema
 from clients.files.files_schema import CreateFileRequestSchema
+from clients.courses.courses_schema import CreateCourseRequestSchema
 
 
 public_users_client = get_public_users_client()
@@ -30,17 +31,17 @@ create_file_request = CreateFileRequestSchema(
     directory='courses',
     upload_file='./testdata/files/image.png'
 )
-create_file_respons = files_client.create_file(create_file_request)
-print('Create file data', create_file_respons)
+create_file_response = files_client.create_file(create_file_request)
+print('Create file data', create_file_response)
 
-create_course_request = CreateCourseRequestDict(
+create_course_request = CreateCourseRequestSchema(
     title='Python',
-    maxScore=100,
-    minScore=10,
+    max_score=100,
+    min_score=10,
     description='Python API course',
-    estimatedTime='2 weeks',
-    previewFileId=create_file_respons.file.id,
-    createdByUserId=create_user_response.user.id
+    estimated_time='2 weeks',
+    preview_file_id=create_file_response.file.id,
+    created_by_user_id=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
