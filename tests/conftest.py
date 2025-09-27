8b3a5a55bb628cf5983d8903d9f6220864e509fe
e-kovaleva <fullmoon010191@gmail.com>
2025-09-27 11:38:27 +0300

    8.5 Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUsersRequestSchema, CreateUserResponseSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.private_http_builder import AuthenticationUserSchema
 import pytest
 from pydantic import BaseModel, EmailStr
 
@@ -16,6 +18,13 @@ class UserFixture(BaseModel):
     @property
     def password(self) -> str:
         return self.request.password
+    
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:
+        return AuthenticationUserSchema(
+            email=self.request.email,
+            password=self.request.password
+        )
 
 
 @pytest.fixture
@@ -33,3 +42,8 @@ def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUsersRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)
+
+
+@pytest.fixture
+def private_users_client(function_user: UserFixture):
+    return get_private_users_client(function_user.authentication_user)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,9 @@
 from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUsersRequestSchema, CreateUserResponseSchema
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.users_schema import CreateUsersRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 from http import HTTPStatus
 import pytest
 
@@ -16,5 +17,17 @@ def test_create_user(public_users_client: PublicUsersClient):
 
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
+
+    validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: PublicUsersClient):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data, function_user.response)
 
     validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
