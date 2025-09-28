4ba14acde3fc4e15a497387e1f930a9aa71014fa
e-kovaleva <fullmoon010191@gmail.com>
2025-09-28 18:51:45 +0300

    8.7 Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,14 +4,16 @@ from clients.users.users_schema import CreateUsersRequestSchema, CreateUserRespo
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
 from http import HTTPStatus
 import pytest
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUsersRequestSchema()
+@pytest.mark.parametrize('email', ['mail.ru', 'gmail.com', 'example.com'])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    request = CreateUsersRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
