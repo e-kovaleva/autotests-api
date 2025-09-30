c6efaf1fffbbc438f19b1559dcf26e255bcb532d
e-kovaleva <fullmoon010191@gmail.com>
2025-09-30 20:00:58 +0300

    9.1 Практика реализации фикстур

--- fixtures/exercises.py
+++ fixtures/exercises.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from clients.exercises.exercises_client import ExercisesClient, get_exercises_client
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
 from fixtures.users import UserFixture
+from fixtures.courses import CourseFixture
 
 
 class ExerciseFixture(BaseModel):
@@ -16,7 +17,10 @@ def exercises_client(function_user: UserFixture) -> ExercisesClient:
 
 
 @pytest.fixture
-def function_exercise(exercises_client: ExercisesClient) -> ExerciseFixture:
-    request = CreateExerciseRequestSchema()
+def function_exercise(
+    exercises_client: ExercisesClient,
+    function_course: CourseFixture
+) -> ExerciseFixture:
+    request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
     response = exercises_client.create_exercise(request)
     return ExerciseFixture(request=request, response=response)
