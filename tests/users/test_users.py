e0fe71553b0583eb75f6bd0b2bc0d4295e78b523
e-kovaleva <fullmoon010191@gmail.com>
2025-09-30 19:33:15 +0300

    9.1 Рефакторинг и подготовка к написанию API автотестов

---
+++ tests/users/test_users.py
@@ -0,0 +1,37 @@
+from clients.users.public_users_client import PublicUsersClient
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.users_schema import CreateUsersRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.assertions.base import assert_status_code
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
+from http import HTTPStatus
+import pytest
+
+
+@pytest.mark.users
+@pytest.mark.regression
+class TestUsers:
+    @pytest.mark.parametrize('email', ['mail.ru', 'gmail.com', 'example.com'])
+    def test_create_user(self, email: str, public_users_client: PublicUsersClient):
+        request = CreateUsersRequestSchema(email=fake.email(domain=email))
+        response = public_users_client.create_user_api(request)
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_user_response(request, response_data)
+
+        validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
+
+    def test_get_user_me(
+        self,
+        private_users_client: PrivateUsersClient,
+        function_user: PublicUsersClient
+    ):
+        response = private_users_client.get_user_me_api()
+        response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_user_response(response_data, function_user.response)
+
+        validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
